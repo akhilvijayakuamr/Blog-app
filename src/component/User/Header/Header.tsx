332da3afd0330abe0cac15c4cefe23f4d4fda28e
Akhil Vijayakumar <akhilvijayakumar333@gmail.com>
2025-01-05 12:10:35 +0530

    unwanted import is removed

--- src/component/User/Header/Header.tsx
+++ src/component/User/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Header.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { useDispatch } from 'react-redux';
